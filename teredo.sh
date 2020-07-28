335fc74b2d4ae27e57c13e1dfbeabc434e060403
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-07-29 02:42:43 +0300

    Fix for host network

--- teredo.sh
+++ teredo.sh
@@ -4,12 +4,12 @@ if [ -f "/var/run/netns/container" ]
 then
     exec_command="ip netns exec container"
 else
-    exec_command="exec"
+    exec_command="bash -c"
 fi
 
-printf "Enabling IPv6 " ; $exec_command sysctl net.ipv6.conf.all.disable_ipv6=0 > /dev/null && echo "OK." || ( printf "ERR"; err_on_exit="yes" )
-printf "Enabling IPv6 by default " ; $exec_command sysctl net.ipv6.conf.default.disable_ipv6=0 > /dev/null && echo "OK." || ( printf "ERR"; err_on_exit="yes" )
-printf "Enabling IPv6 by local " ; $exec_command sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null && echo "OK." || ( printf "ERR"; err_on_exit="yes" )
+printf "Enabling IPv6 " ; eval $exec_command sysctl net.ipv6.conf.all.disable_ipv6=0 > /dev/null && echo "OK." || ( printf "ERR"; err_on_exit="yes" )
+printf "Enabling IPv6 by default " ; eval $exec_command sysctl net.ipv6.conf.default.disable_ipv6=0 > /dev/null && echo "OK." || ( printf "ERR"; err_on_exit="yes" )
+printf "Enabling IPv6 by local " ; eval $exec_command sysctl net.ipv6.conf.lo.disable_ipv6=0 > /dev/null && echo "OK." || ( printf "ERR"; err_on_exit="yes" )
 
 if [ "$err_on_exit" == "yes" ]
 then
